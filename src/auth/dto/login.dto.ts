29aaf1c09c15aa543b7b0142cb5f416e62964e94
Ana Helena <anahelenarp@hotmail.com>
2026-01-11 16:04:30 -0300

    refactor: remove min lenght validation on login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail({}, { message: 'Invalid email format' })
@@ -7,6 +7,5 @@ export class LoginDto {
 
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password: string;
 }
