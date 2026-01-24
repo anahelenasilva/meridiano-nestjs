9fbb59d00cdcbc21a204feb14848fe7cc05ac7f0
Ana Helena <anahelenarp@hotmail.com>
2026-01-24 20:07:24 -0300

    feat: US-003 - Update barrel export for email module

--- libs/email/index.ts
+++ libs/email/index.ts
@@ -1,6 +1,3 @@
-export * from './email.module';
-export * from './email.service';
-export * from './interfaces/email-provider.interface';
-export * from './interfaces/send-email-options.interface';
-export * from './providers/mailgun.provider';
+export { EmailModule } from './email.module';
+export { EmailService } from './email.service';
 
