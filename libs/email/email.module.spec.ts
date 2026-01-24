e484660727a287b917ea7dcd41eb3fe4b901dc1c
Ana Helena <anahelenarp@hotmail.com>
2026-01-24 20:39:18 -0300

    test: improve clean up

--- libs/email/email.module.spec.ts
+++ libs/email/email.module.spec.ts
@@ -4,9 +4,15 @@ import { EmailService } from './email.service';
 
 describe('EmailModule', () => {
   let module: TestingModule;
+  let originalEmailProvider: string | undefined;
+  let originalMailgunApiKey: string | undefined;
+  let originalMailgunDomain: string | undefined;
 
   beforeEach(async () => {
-    const originalEnv = process.env.EMAIL_PROVIDER;
+    originalEmailProvider = process.env.EMAIL_PROVIDER;
+    originalMailgunApiKey = process.env.MAILGUN_API_KEY;
+    originalMailgunDomain = process.env.MAILGUN_DOMAIN;
+
     process.env.EMAIL_PROVIDER = 'mailgun';
     process.env.MAILGUN_API_KEY = 'test-key';
     process.env.MAILGUN_DOMAIN = 'test-domain.com';
@@ -14,9 +20,25 @@ describe('EmailModule', () => {
     module = await Test.createTestingModule({
       imports: [EmailModule.forRoot()],
     }).compile();
+  });
 
-    if (originalEnv) {
-      process.env.EMAIL_PROVIDER = originalEnv;
+  afterEach(() => {
+    if (originalEmailProvider !== undefined) {
+      process.env.EMAIL_PROVIDER = originalEmailProvider;
+    } else {
+      delete process.env.EMAIL_PROVIDER;
+    }
+
+    if (originalMailgunApiKey !== undefined) {
+      process.env.MAILGUN_API_KEY = originalMailgunApiKey;
+    } else {
+      delete process.env.MAILGUN_API_KEY;
+    }
+
+    if (originalMailgunDomain !== undefined) {
+      process.env.MAILGUN_DOMAIN = originalMailgunDomain;
+    } else {
+      delete process.env.MAILGUN_DOMAIN;
     }
   });
 
