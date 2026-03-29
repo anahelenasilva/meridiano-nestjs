899ccb4fb48b928b44a93ed97a198c4462b9352b
Ana Helena <anahelenarp@hotmail.com>
2026-03-29 11:07:56 -0300

    refactor: remove unused endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Public } from '@libs/auth';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Public()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor() { }
 
   @Public()
   @Get('api/health')
